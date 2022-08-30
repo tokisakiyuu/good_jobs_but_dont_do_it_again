1e391fd380242af277582606db616a63f7d50b0b
zhaoliping <tokisakiyuu@qq.com>
2022-08-30 17:15:49 +0800

    fixs problem can not use enter key to submit in qq

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,16 +5,15 @@ import { useRouter } from 'next/router'
 import { css } from '@emotion/css'
 import {
   Container,
-  TextField
+  TextField,
+  Button
 } from '@mui/material'
 
 const Home: NextPage = () => {
   const [isError, setError] = useState(false)
+  const [keyword, setKeyword] = useState<string>('')
   const router = useRouter()
-  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    const data = new FormData(e.currentTarget)
-    const keyword = data.get('keyword')
+  const onSubmit = () => {
     if (!keyword) {
       return setError(true)
     }
@@ -28,18 +27,20 @@ const Home: NextPage = () => {
       </Head>
       <Container maxWidth='sm'>
         <div className={inputKeywordWrapperStyle}>
-          <form onSubmit={onSubmit}>
-            <TextField
-              name='keyword'
-              label='动词'
-              variant="outlined"
-              color='success'
-              autoFocus
-              focused
-              helperText={isError ? '不能为空' : '输入一个动词并按确认'}
-              error={isError}
-            />
-          </form>
+          <TextField
+            value={keyword}
+            label='动词'
+            variant="outlined"
+            color='success'
+            autoFocus
+            focused
+            helperText={isError ? '不能为空' : '输入一个动词并按确认'}
+            error={isError}
+            onInput={e => setKeyword((e.target as HTMLInputElement).value)}
+          />
+          <div className='confirm-btn'>
+            <Button variant='contained' onClick={onSubmit}>确定</Button>
+          </div>
         </div>
       </Container>
     </>
@@ -48,7 +49,13 @@ const Home: NextPage = () => {
 
 const inputKeywordWrapperStyle = css({
   width: 160,
-  margin: '100px auto'
+  margin: '100px auto',
+  '.confirm-btn': {
+    marginTop: 20,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 })
 
 export default Home
